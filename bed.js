854ede3d20a6ef10d5f558bec7fd5d781aa0a42b
varnikap <78656240+varnikap@users.noreply.github.com>
2022-02-25 17:37:13 -0500

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,35 @@
+status="";
+
+function preload()
+{
+    img=loadImage('bed.jpg');
+}
+
+function setup()
+{
+    canvas= createCanvas(380, 380);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded());
+    document.getElementById("status").innerHTML = "Detecting Objects"; 
+
+}
+
+function modelLoaded()
+{
+   console.log("Model Loaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
